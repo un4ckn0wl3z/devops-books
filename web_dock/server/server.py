6f3dddad0eab21bf9409eaf3f7d2f3390ab45178
anuwat.devops <root@unknownclub.net>
2021-02-15 04:26:21 +0000

    init

---
+++ web_dock/server/server.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+import http.server
+import socketserver
+
+handler = http.server.SimpleHTTPRequestHandler
+
+
+with socketserver.TCPServer(("", 8083), handler) as httpd:
+
+    httpd.serve_forever()
